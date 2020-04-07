f820aacfd9b74923da0692cc20423b6513a70e4e
zeeyn <zeeyn@ddaproduction.com>
2020-04-07 14:15:53 +0300

    Laravel View usage in interface module

--- modules/config_module/ec.module.php
+++ modules/config_module/ec.module.php
@@ -1,4 +1,17 @@
 <?php
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\FileViewFinder;
+use \Illuminate\Support\Facades\View;
 
-//$template = file_get_contents(dirname(__FILE__) . '/ec.interface.tpl');
+
+
+
+$filesystem = new Filesystem();
+$viewFinder = new FileViewFinder($filesystem, [EVO_CORE_PATH . 'vendor/zeeyn/evocms-eternal-cookies/modules/view/']);
+View::setFinder($viewFinder);
+
+
+
+echo View::make('ec.interface.blade.php');
+//$template = file_get_contents(dirname(__FILE__) . '/ec.interface.tpl')
 //echo $template;
