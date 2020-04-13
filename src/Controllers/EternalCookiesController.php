7944eec4a4f7e81a2d3bbd84088d9b9a695d6da0
zeeyn <zeeyn@ddaproduction.com>
2020-04-13 16:12:40 +0300

    Started core projecting

--- src/Controllers/EternalCookiesController.php
+++ src/Controllers/EternalCookiesController.php
@@ -1,15 +1,73 @@
 <?php
 namespace EvolutionCMS\ZeeyN\EternalCookies\Controllers;
 
+use EvolutionCMS;
+
 class EternalCookiesController
 {
+    private $evo;
+    private $window;
+    private $styles;
+    private $scripts;
 
     function __construct()
     {
+        $this->evo = EvolutionCMS();
+    }
+
+    function __clone()
+    {}
+
+    public function renderWindow():string
+    {
+        return $this->window;
+    }
+
+    public function getScripts():string
+    {
+        return $this->scripts;
+    }
+
+    public function getStyles():string
+    {
+        return $this->styles;
+    }
+
+    private function prepareStyles()
+    {
+        $this->styles = 'styles';
+    }
+
+    private function prepareScripts()
+    {
+        $this->scripts = 'scripts';
+    }
+
+    private function prepareWindow()
+    {
+        $this->window = 'window';
+    }
+
+    public function isAccepted()
+    {
+        //todo Проверка включенности пакета
+        return true;
+    }
+
+    public function makeAlive()
+    {
+        //todo Полный рендер всего
+        $this->prepareWindow();
+        $this->prepareStyles();
+        $this->prepareScripts();
 
 
 
     }
+
+
+
+
 
     //TODO Обработку конфигураций из модели SiteModule
     //TODO Сборку окна печенек
--- src/EternalCookiesServiceProvider.php
+++ src/EternalCookiesServiceProvider.php
@@ -4,20 +4,27 @@ namespace EvolutionCMS\ZeeyN\EternalCookies;
 use EvolutionCMS;
 use EvolutionCMS\ServiceProvider;
 use EvolutionCMS\ZeeyN\EternalCookies\Controllers\EternalCookiesController;
+use Illuminate\Support\Facades\Blade;
 
 class EternalCookiesServiceProvider extends ServiceProvider
 {
     protected $evo;
+    protected $ec;
 
     public function register()
     {
         $this->evo = EvolutionCMS();
+        $this->evo->regClientCSS('<style>TEST</style>');
+        $this->evo->regClientScript('<script>console.log("I\'m here")</script>');
+        $this->evo->regClientHTMLBlock('<div>TESTING</div>');
+        $this->ec = new EternalCookiesController();
         $this->app->registerModule(
-                'EternalCookies',
-                dirname(__DIR__) . '/modules/ec.module.php'
+            'EternalCookies',
+            dirname(__DIR__) . '/modules/ec.module.php'
         );
-        $this->evo->addDataToView([
-                'EternalCookies' => new EternalCookiesController()
-                                  ]);
+        if($this->ec->isAccepted())
+        {
+            $this->ec->makeAlive();
+        }
     }
 }
